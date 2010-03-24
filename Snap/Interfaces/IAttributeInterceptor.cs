d08a971c3dcb276890b77d978a9199ad476a57ea
Tyler Brinks <tylerbrinks@hotmail.com>
2010-03-24 08:58:38 -0600

    Fixed the Castle implementation.  Far better binding syntax as well.

--- Snap/Interfaces/IAttributeInterceptor.cs
+++ Snap/Interfaces/IAttributeInterceptor.cs
@@ -3,6 +3,9 @@ using Castle.Core.Interceptor;
 
 namespace Snap
 {
+    /// <summary>
+    /// Defines a mapping between an interceptor and the attribute the interceptor responds to.
+    /// </summary>
     public interface IAttributeInterceptor : IInterceptor
     {
         Type TargetAttribute { get; set; }
