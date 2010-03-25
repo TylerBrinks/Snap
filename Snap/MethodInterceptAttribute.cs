6634ad03e4852219df85177869343d07e263f812
Tyler Brinks <tylerbrinks@hotmail.com>
2010-03-25 00:24:22 -0600

    PseduoInterceptors Rock!  Clever end-around Castle.

--- Snap/MethodInterceptAttribute.cs
+++ Snap/MethodInterceptAttribute.cs
@@ -8,15 +8,26 @@ namespace Snap
     [AttributeUsage(AttributeTargets.Method, AllowMultiple=false, Inherited=true)]
     public class MethodInterceptAttribute : Attribute, IInterceptAttribute
     {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="MethodInterceptAttribute"/> class.
+        /// </summary>
         public MethodInterceptAttribute() : this(0)
         {
         }
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="MethodInterceptAttribute"/> class.
+        /// </summary>
+        /// <param name="order">The order.</param>
         public MethodInterceptAttribute(int order)
         {
             Order = order;
         }
 
+        /// <summary>
+        /// Gets or sets the order.
+        /// </summary>
+        /// <value>The order.</value>
         public int Order { get; set; }
     }
 }
